9124e75eade9e4092f7fc343c90d805eaf40a60c
Alexander Cochrane <acochrane@almightycorp.com>
2016-10-05 16:22:51 -0300

    made some buttons

--- app/src/main/java/ca/alexcochrane/quizbuilder/MainActivity.java
+++ app/src/main/java/ca/alexcochrane/quizbuilder/MainActivity.java
@@ -3,11 +3,21 @@ package ca.alexcochrane.quizbuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
